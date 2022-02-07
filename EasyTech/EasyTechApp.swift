6f43a58a9a770a83ceaf22c19fa1fe6301fef818
iblessme <vldikk12@gmail.com>
2022-02-07 12:51:11 +0300

    Initial Commit

---
+++ EasyTech/EasyTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTechApp.swift
+//  EasyTech
+//
+//  Created by iBlessme on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct EasyTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
